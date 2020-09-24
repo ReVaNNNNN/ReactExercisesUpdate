6852fdbc596c1e67e5463c0d6b7a5942e2bfede9
revan <revannn5480@gmail.com>
2020-09-24 15:59:03 +0200

    Second mini App - change

--- ReactPodstawyUpdate/second/app.js
+++ ReactPodstawyUpdate/second/app.js
@@ -1,5 +1,28 @@
 const App = () => {
-   
+    const [value, setValue] = React.useState('');
+
+    const onClickHandler = () => {
+        setValue('');
+    }
+
+    const onChangeHandler = (e) => {
+        setValue(e.target.value)
+    }
+
+    return (
+        <>
+            <input 
+                value={value} 
+                placeholder="Wpisz..." 
+                onChange={onChangeHandler} 
+                type="text"
+            />
+            <button onClick={onClickHandler}>
+                Reset
+            </button>
+            <h1 className="title">{value.toUpperCase()}</h1>
+        </>
+    );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
