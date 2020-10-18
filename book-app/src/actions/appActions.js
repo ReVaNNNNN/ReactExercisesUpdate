da5035b262691a7f0faacdb79948f2d6add092a2
ReVaN <revannn5480@gmail.com>
2020-10-18 09:21:03 +0200

    book app - Redux

---
+++ book-app/src/actions/appActions.js
@@ -0,0 +1,15 @@
+export const ADD = 'ADD';
+export const DELETE = 'DELETE';
+export const EDIT = 'EDIT';
+
+const add = ({
+    
+})
+{
+    type: ADD,
+    payload: {
+        author: 'Zbigniew Krawiec',
+        rate: 4,
+        comment: 'Trochę długa i miejscami przynudza ale ogólnie dobra książka'
+    }
+}
